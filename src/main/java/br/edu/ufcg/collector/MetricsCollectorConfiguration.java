aceda6939dec9900601c3e167aff1f8f8569e4b9
ramonbezerra <ramon.bezerra@outlook.com>
2022-06-23 11:27:39 -0300

    update for library example

--- src/main/java/br/edu/ufcg/collector/MetricsCollectorConfiguration.java
+++ src/main/java/br/edu/ufcg/collector/MetricsCollectorConfiguration.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Collector.MetricFamilySamples;
 
-public class MetricsCollector {
+@Configuration
+@ComponentScan
+public class MetricsCollectorConfiguration {
     @Autowired
     private CollectorRegistry collectorRegistry;
 
-    public List<Object> hello() {
+    @Bean
+    public List<Object> metricSamples() {
         Enumeration<MetricFamilySamples> samples = this.collectorRegistry.metricFamilySamples();
         List<Object> samplesList = new ArrayList<>();
         while (samples.hasMoreElements()) {
